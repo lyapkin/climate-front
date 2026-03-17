218d85dad94c169a46177d85c58e169de74b363a
lyapkin <lyapkin992@gmail.com>
2026-03-17 13:26:24 +0500

    change rating to link

--- src/entities/review/ui/Stars/Stars.tsx
+++ src/entities/review/ui/Stars/Stars.tsx
@@ -1,7 +1,6 @@
 import { HTMLAttributes } from "react";
 import s from "./styles.module.css";
 import cn from "classnames";
-import Link from "next/link";
 
 const Stars = ({ rate, className }: StarsProps) => {
   const round = Math.floor(rate);
@@ -23,17 +22,7 @@ const Stars = ({ rate, className }: StarsProps) => {
       ></span>
     );
   });
-  return (
-    <Link
-      href={
-        "https://www.avito.ru/brands/4a3f3e00703d4cb99a7d3b8249f1b0b3/all/predlozheniya_uslug?sellerId=4abe76a4404b33dc9e3b98d67b5bc1a1"
-      }
-      target="_blank"
-      className={cn(s.stars, className)}
-    >
-      {stars}
-    </Link>
-  );
+  return <span className={cn(s.stars, className)}>{stars}</span>;
 };
 
 interface StarsProps extends HTMLAttributes<HTMLElement> {
--- src/widgets/review/ui/Rating/Rating.tsx
+++ src/widgets/review/ui/Rating/Rating.tsx
@@ -2,16 +2,23 @@ import React, { HTMLAttributes } from "react";
 import s from "./styles.module.css";
 import cn from "classnames";
 import { Stars } from "@/src/entities/review";
+import Link from "next/link";
 
 const Rating = ({ className }: HTMLAttributes<HTMLElement>) => {
   const rate = 5.0;
 
   return (
-    <div className={cn(s.rating, className)}>
+    <Link
+      href={
+        "https://www.avito.ru/brands/4a3f3e00703d4cb99a7d3b8249f1b0b3/all/predlozheniya_uslug?sellerId=4abe76a4404b33dc9e3b98d67b5bc1a1"
+      }
+      target="_blank"
+      className={cn(s.rating, className)}
+    >
       <Stars rate={rate} className={s.rating__stars} />
-      <div className={s.rating__num}>{rate.toFixed(1)}</div>
-      <p className={s.rating__title}>Наш рейтинг на Авито</p>
-    </div>
+      <span className={s.rating__num}>{rate.toFixed(1)}</span>
+      <span className={s.rating__title}>Наш рейтинг на Авито</span>
+    </Link>
   );
 };
 

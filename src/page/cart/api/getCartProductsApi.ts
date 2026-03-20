dd76498f6591473425e2fab961e035fb1b3ad5df
lyapkin <lyapkin992@gmail.com>
2026-03-20 11:32:23 +0500

    clean

--- src/page/cart/api/getCartProductsApi.ts
+++ src/page/cart/api/getCartProductsApi.ts
@@ -1,7 +1,7 @@
-import { ProductBase, ProductCartResponse } from "@/src/entities/product";
+import { ProductBase, ProductCartResponse } from "@/src/entities/product/model";
 
 export const getCartProductsApi = async (
-  ids: ProductBase["id"][]
+  ids: ProductBase["id"][],
 ): Promise<ProductCartResponse[]> => {
   if (ids.length == 0) return [];
 
@@ -12,37 +12,4 @@ export const getCartProductsApi = async (
   const res = await fetch(url);
 
   return await res.json();
-
-  // console.log(ids);
-
-  // return [
-  //   {
-  //     id: 1,
-  //     name: "Сплит система BOSCH WBN6000-18H RN S5700 одноконтурный",
-  //     slug: "slug4",
-  //     stock: true,
-  //     attrs: [
-  //       { id: 1, attr: "Марка", value: "Bosh" },
-  //       { id: 2, attr: "Артикул", value: "7736900199RU" },
-  //       { id: 3, attr: "Мощность", value: "– 24 кВт" },
-  //     ],
-  //     img: "/images/product/1.png",
-  //     prices: [{ id: 1, price: 14534, discount: 20, name: "" }],
-  //     code: "FX8932423",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Сплит система BOSCH WBN6000-18H RN S5700 одноконтурный",
-  //     slug: "slug4",
-  //     stock: true,
-  //     attrs: [
-  //       { id: 1, attr: "Марка", value: "Bosh" },
-  //       { id: 2, attr: "Артикул", value: "7736900199RU" },
-  //       { id: 3, attr: "Мощность", value: "– 24 кВт" },
-  //     ],
-  //     img: "/images/product/1.png",
-  //     prices: [{ id: 2, price: 11530, discount: 15, name: "" }],
-  //     code: "FX8932424",
-  //   },
-  // ];
 };
